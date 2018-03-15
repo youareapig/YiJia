ccbe71d4c5a3e2f3c0ac7bd1fdde25fdeee140ef
DELL <840855165@qq.com>
2018-03-15 09:10:32 +0800

    测试

--- app/src/main/java/com/yijia/zhenglei/KjxqActivity.java
+++ app/src/main/java/com/yijia/zhenglei/KjxqActivity.java
@@ -32,6 +32,6 @@ public class KjxqActivity extends AutoLayoutActivity {
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }//chongxintijiao
+        }
     }
 }
